901a9054f2f6becef78f3881692be153f6c559e1
abdi mohamed <aabdymohamed@gmail.com>
2022-08-08 22:54:31 +0200

    start

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Register } from './components/register/Register';
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Register/>
       <StatusBar style="auto" />
+
     </View>
   );
 }
---
+++ components/register/Register.jsx
@@ -0,0 +1,100 @@
+// Formik x React Native example
+import React from 'react';
+import { Button, TextInput, View, StyleSheet} from 'react-native';
+import { Formik } from 'formik';
+
+export const Register = props => (
+  <Formik
+    initialValues={{email:"" ,userName:"" , password:""}}
+    onSubmit={values => console.log(values.email,values.userName, values.password)}
+  >
+    {({ handleChange, handleBlur, handleSubmit, values }) => (
+      <View>
+        
+        <TextInput style={styles.TextInput}
+          placeholder="Email"
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
+          value={values.email}
+        />
+         <TextInput style={styles.TextInput}
+          placeholder="username"
+          onChangeText={handleChange('userName')}
+          onBlur={handleBlur('userName')}
+          value={values.userName}
+        />
+         <TextInput style={styles.TextInput}
+         placeholder="password"
+          onChangeText={handleChange('password')}
+          onBlur={handleBlur('password')}
+          value={values.password}
+        />
+        <Button onPress={handleSubmit} title="Submit" />
+
+      </View>
+      
+    )}
+  </Formik>
+);
+
+
+const styles = StyleSheet.create({
+  TextInput:{
+    border:"solid 1px grey",
+    padding:"2px",
+    margin:"2px"
+  },
+  // container:{
+  //   justifyContent:"center"
+  // }
+})
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import { useState } from 'react'
+// import { StyleSheet, Text, View } from 'react-native';
+// import { Button } from 'react-native';
+
+// export default function input(props) {
+// const [user, setUser] = useState("")
+// const [pwd, setPwd] = useState("")
+// // const [show,setShow]= useState(false)
+// // let displayShit = ()=>{
+// // return <h1>välkommen {user}</h1>
+// // }
+// // console.log(user)
+// // console.log(pwd)
+
+//   return (
+//     <div>
+//         <form >
+//         <input onChange={e=>setUser(e.target.value)} type="text" value={user} name="user" id="user" />
+//         <input onChange={e=>setPwd(e.target.value)} autoComplete="current-pasword" type="password" value={pwd} name="pwd" id="pwd" />
+//         </form>
+
+  
+        
+//     </div>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       display:'flex',
+//       justifyContent: "spaceBetween",
+//       alignItems:"stretch"  ,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+    
+//   });
